d40d8c39a708b2802b580b7a446c5af0ab64af95
denizsurmeli <denizsurmeli@icloud.com>
2023-03-05 02:00:11 +0300

    fix: improve prototype

--- ws1/prototype.py
+++ ws1/prototype.py
@@ -1,10 +1,10 @@
+import multiprocessing
 import subprocess
 import json
 import logging
 import socket
 import datetime
 import threading
-
 
 PORT = 12345
 
@@ -53,30 +53,53 @@ class Netchat:
 
         self.dict_lock = threading.Lock()
 
-        t1 = threading.Thread(target=self.listen_network)
-        t2 = threading.Thread(target=self.discover_peers)
-        t3 = threading.Thread(target=self.listen_user)
+        self.t1 = threading.Thread(target=self.listen_network)
+        self.t2 = threading.Thread(target=self.discover_peers)
+        self.t3 = threading.Thread(target=self.listen_user)
 
-        t1.start()
+        self.t1.start()
 
         # first discover peers, then accept actions from the user
-        t2.start()
-        t2.join()
-        t3.start()
+        self.t2.start()
+        self.t2.join()
+        self.t3.start()
 
         # these are practically alive until the program terminates
-        t1.join()        
-        t3.join()
+        self.t1.join()        
+        self.t3.join()
 
+    def get_ip_by_name(self, name: str):
+        for peer in self.peers:
+            if self.peers[peer] == name:
+                return peer
+        return None
+    
     def listen_user(self):
         while True:
             line = input()
-            if line == "exit":
+            if line == ":exit":
                 self.shutdown()
                 break
-            else:
-                print("listen_user:", line)
-                # self.send_message(line)
+            if line == ":peers":
+                print("IP:\t\tName:")
+                for peer in self.peers:
+                    print(f"{peer}\t{self.peers[peer]}")
+            if line.startswith(":send"):
+                try:
+                    name, content = line.split()[1:]
+                    name = name.strip()
+                    content = content.strip()
+                    ip = self.get_ip_by_name(name)
+                    message = MESSAGE.copy()
+                    message['ip'] = self.whoami.ip
+                    message['content'] = content
+
+                    if ip is None:
+                        print(f"Peer with name {name} not found.")
+                    else:
+                        self.send_message(message, ip)
+                except:
+                    print("Invalid command. Usage: :send name message")
     
     def shutdown(self):
         byebye_message = LEAVE_MESSAGE.copy()
@@ -115,11 +138,6 @@ class Netchat:
             if output:
                 self.process_message(output.strip())
     
-    # def resolve_sender(self, ip:str) -> str:
-    #     """
-    #         Resolves the name of the sender.
-    #     """
-
         
     def process_message(self, message: str):
         try:
@@ -131,15 +149,15 @@ class Netchat:
                 ack['ip'] = self.whoami.ip
                 ack['name'] = self.whoami.name
 
-                self.send_message(ack, name=message['name'], ip=message['ip'])
-                self.add_peer(Peer(message['ip'], message['name']), self.dict_lock)
+                self.send_message(ack, ip=message['ip'])
+                self.add_peer(message['ip'], message['name'], self.dict_lock)
             
             if message['type'] == 'message':
-                print(f"[{datetime.datetime.now()}] | FROM: ({message['ip']}): {message['content']}")
+                print(f"[{datetime.datetime.now()}] | FROM: {self.peers[message['ip']]}({message['ip']}): {message['content']}")
 
             if message['type'] == 'aleykumselam':
                 logging.info(f"Peer found. ip: {message['ip']} name: {message['name']} ")
-                self.add_peer(Peer(message['ip'], message['name']), self.dict_lock)
+                self.add_peer(message['ip'], message['name'], self.dict_lock)
             
             # this is not in the spec but definetely needed
             if message['type'] == 'byebye':
@@ -149,42 +167,25 @@ class Netchat:
         except Exception as e:
             print(e)
             logging.error("Error while processing the message.")
-
-
-    def send_message(self, content:str, name:str):
-        receiver_ip: str = self.peers[name]
-        message = MESSAGE.copy()
-        message['content'] = content
-        message['ip'] = self.ip 
-        self.send_message(message, receiver_ip)
     
-    def send_message(self, message: dict[str], name:str, ip:str=None):
-        if ip is None:
-            ip = self.peers[name]
-            self.send_message(message, ip)
-        else:
-            self.send_message(message, ip)
-    
-    def send_message(self, message: dict[str], ip:str, timeout:int= 1):
+    def send_message(self, message: dict[str], ip:str, timeout:int= 0.02):
         try:
             logging.info(f"Sending \"{message['type']}\" message to {ip}")
-            subprocess.check_output(['nc', ip, str(PORT)], input=bytes(json.dumps(message), 'utf-8'), timeout=0.02)
+            process = subprocess.run([f'echo {json.dumps(message)} | nc -w {str(timeout)} {ip} {str(PORT)}'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
             logging.info(f"Message sent. ip: {ip}")
         except subprocess.TimeoutExpired:
             pass
         except Exception as e:
+            print(e)
             logging.error(f"Error while sending the message. ip: {ip}") 
 
-    def add_peer(self, peer:Peer, lock:threading.Lock):
-        if peer['ip'] not in self.peers:
+    def add_peer(self, ip, name, lock:threading.Lock):
+        if ip not in self.peers:
             lock.acquire()
-            self.peers[peer['ip']] = peer['name']
+            self.peers[ip] = name
             lock.release()
-            logging.info(f"Peer added. ip: {peer['ip']} name: {peer['name']}")
+            logging.info(f"Peer added. ip:{ip} name: {name}")
         
-
-
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
